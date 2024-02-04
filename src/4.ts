371a9c02264f0899e1027a5447292f1f87e1587f
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2024-02-04 21:57:31 +0200

    up

--- src/4.ts
+++ src/4.ts
@@ -1,10 +1,51 @@
-const key = new Key();
+class Key{
+private signature: number = Math.random();
+getSignature(): number {
+    return this.signature;
+}
+}
+class Person {
+    private key: Key;
+    constructor(key:Key){
+        this.key;
+    }
+    getKey(): Key{
+        return this.key
+    }
+}
 
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    protected residents: Person[] = [];
+
+    constructor(key: Key){
+        this.door = false;
+        this.key = key;
+    }
+
+    abstract openDoor(key:Key): void;
+
+    comeIn(person:Person): void{
+        if (this.door) {
+            this.residents.push(person);
+            console.log(`Welcome.${this.residents}`)
+        }
+    }
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log(`Welcome.${this.residents}`)
+        }
+    }
+}
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
 
